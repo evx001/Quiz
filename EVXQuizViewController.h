70863f6e095a88881d7d7e30f854b3f4cfe4f4d1
evx001 <eviexeris@gmail.com>
2014-05-27 17:59:32 -0400

    first Commit w/ Controllers and .xib

---
+++ EVXQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  EVXQuizViewController.h
+//  Quiz
+//
+//  Created by evx on 5/27/14.
+//  Copyright (c) 2014 evxyz001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EVXQuizViewController : UIViewController
+
+@end
---
+++ EVXQuizViewController.m
@@ -0,0 +1,38 @@
+//
+//  EVXQuizViewController.m
+//  Quiz
+//
+//  Created by evx on 5/27/14.
+//  Copyright (c) 2014 evxyz001. All rights reserved.
+//
+
+#import "EVXQuizViewController.h"
+
+@interface EVXQuizViewController ()
+
+@end
+
+@implementation EVXQuizViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
